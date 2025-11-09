9bf3f4398bcc60944eb3852fc05313398f722715
nickhildpac <162016801+nickhildpac@users.noreply.github.com>
2025-11-09 12:36:36 +0530

    added microservices

---
+++ 02-microservies-go/services/trip-service/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/services/trip-service/internal/infrastructure/repository"
+	"ride-sharing/services/trip-service/internal/service"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+
+	inmemRepo := repository.NewInmemRepository()
+	svc := service.NewService(inmemRepo)
+
+	fare := &domain.RideFareModel{
+		UserID: "32",
+	}
+	t, err := svc.CreateTrip(ctx, fare)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	log.Println(t)
+
+	// keep program running for now
+	for {
+		time.Sleep(time.Second)
+	}
+}
---
+++ 02-microservies-go/services/trip-service/internal/domain/ride_fare.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type RideFareModel struct {
+	ID                primitive.ObjectID
+	UserID            string
+	PackageSlug       string // van, luxury, sedan
+	TotalPriceInCents float64
+}
